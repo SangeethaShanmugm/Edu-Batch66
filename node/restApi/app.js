6a6404f7976663c3b7ca73c7d565ac64902ffc43
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-01-14 16:46:35 +0530

    added api endpoints and nodemon

--- node/restApi/app.js
+++ node/restApi/app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const data = [
+  {
+    "id": 1,
+    "name": "Avengers",
+    "rating": 4.5,
+    "type": "Action",
+  },
+  {
+    id: 2,
+    name: "Jab We Met",
+    rating: 4.7,
+    type: "Romantic",
+  },
+];
+
 //default express endpoint
 app.get("/", (req, res) => {
   res.send("Express Server default page");
@@ -9,6 +24,19 @@ app.get("/", (req, res) => {
 //location endpoint
 app.get("/location", (req, res) => {
   res.send("This is location route");
+});
+//get data
+app.get("/data", (req, res) => {
+  res.send(data);
+});
+
+//get data based on ID
+// req.params - to get the id from
+app.get("/data/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+  const result = data.find((dt) => dt.id == id);
+  res.send(result);
 });
 
 app.listen(PORT, (err) => {
