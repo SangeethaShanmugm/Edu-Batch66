502a11a0169f1babf0a731d2babc28f0bb98f632
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-02-04 16:04:27 +0530

    added env

--- node/restApi/app.js
+++ node/restApi/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongo = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const MongoClient = mongo.MongoClient;
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8000;
+let db;
+
+// console.log(process.env);
+const MONGO_URL = process.env.MONGO_URL;
 
 const data = [
   {
@@ -17,68 +25,68 @@ const data = [
   },
 ];
 
-const locations = [
-  {
-    location_id: 1,
-    location_name: "Ashok Nagar, New Delhi",
-    state_id: 1,
-    state: "Delhi",
-    country_name: "India",
-  },
-  {
-    location_id: 2,
-    location_name: "Jeevan Nagar, New Delhi",
-    state_id: 1,
-    state: "Delhi",
-    country_name: "India",
-  },
-  {
-    location_id: 3,
-    location_name: "Vasant Kunj, New Delhi",
-    state_id: 1,
-    state: "Delhi",
-    country_name: "India",
-  },
-];
+// const locations = [
+//   {
+//     location_id: 1,
+//     location_name: "Ashok Nagar, New Delhi",
+//     state_id: 1,
+//     state: "Delhi",
+//     country_name: "India",
+//   },
+//   {
+//     location_id: 2,
+//     location_name: "Jeevan Nagar, New Delhi",
+//     state_id: 1,
+//     state: "Delhi",
+//     country_name: "India",
+//   },
+//   {
+//     location_id: 3,
+//     location_name: "Vasant Kunj, New Delhi",
+//     state_id: 1,
+//     state: "Delhi",
+//     country_name: "India",
+//   },
+// ];
 
-const quickSearch = [
-  {
-    mealtype_id: 1,
-    mealtype: "Breakfast",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/FVhSTWK/breakfast.jpg",
-  },
-  {
-    mealtype_id: 2,
-    mealtype: "Lunch",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/8rPxkWP/lunch.jpg",
-  },
-  {
-    mealtype_id: 3,
-    mealtype: "Dinner",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/XjzPqYv/dinner.jpg",
-  },
-  {
-    mealtype_id: 4,
-    mealtype: "Snacks",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/wchCHfb/snacks.jpg",
-  },
-  {
-    mealtype_id: 5,
-    mealtype: "Drinks",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/YR0S6fV/drinks.jpg",
-  },
-  {
-    mealtype_id: 6,
-    mealtype: "NightLife",
-    content: "Start your day with exclusive breakfast options",
-    meal_image: "https://i.ibb.co/q1fC2jW/nightlife.jpg",
-  },
-];
+// const quickSearch = [
+//   {
+//     mealtype_id: 1,
+//     mealtype: "Breakfast",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/FVhSTWK/breakfast.jpg",
+//   },
+//   {
+//     mealtype_id: 2,
+//     mealtype: "Lunch",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/8rPxkWP/lunch.jpg",
+//   },
+//   {
+//     mealtype_id: 3,
+//     mealtype: "Dinner",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/XjzPqYv/dinner.jpg",
+//   },
+//   {
+//     mealtype_id: 4,
+//     mealtype: "Snacks",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/wchCHfb/snacks.jpg",
+//   },
+//   {
+//     mealtype_id: 5,
+//     mealtype: "Drinks",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/YR0S6fV/drinks.jpg",
+//   },
+//   {
+//     mealtype_id: 6,
+//     mealtype: "NightLife",
+//     content: "Start your day with exclusive breakfast options",
+//     meal_image: "https://i.ibb.co/q1fC2jW/nightlife.jpg",
+//   },
+// ];
 
 //default express endpoint
 app.get("/", (req, res) => {
@@ -86,14 +94,35 @@ app.get("/", (req, res) => {
 });
 //location endpoint
 app.get("/locations", (req, res) => {
-  res.send(locations);
+  db.collection("location")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
 });
 
 //get mealtype data
 app.get("/quickSearch", (req, res) => {
-  res.send(quickSearch);
+  db.collection("mealType")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
 });
 
+//get restaurant data
+app.get("/restaurant", (req, res) => {
+  db.collection("menu")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+//particular resturant id
 //get data
 app.get("/data", (req, res) => {
   res.send(data);
@@ -108,7 +137,12 @@ app.get("/data/:id", (req, res) => {
   res.send(result);
 });
 
-app.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`Express server listing to port ${PORT}`);
+//Mongodb Connection
+MongoClient.connect(MONGO_URL, (err, client) => {
+  console.log("Mongo is connected");
+  if (err) console.log("Error while connecting");
+  db = client.db("ed66we");
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
