60685695f0ff75f0fbe89136233d76eb71331122
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-02-26 16:49:31 +0530

    added detail page menuList and tabs

--- React/zomato-app/src/components/Listing/ListingApi.js
+++ React/zomato-app/src/components/Listing/ListingApi.js
@@ -50,7 +50,7 @@ export default class ListingApi extends Component {
   }
   componentDidMount() {
     let mealId = this.props.match.params.mealId;
-    // sessionStorage.setItem("mealId", mealId);
+    sessionStorage.setItem("mealId", mealId);
     axios.get(`${url}${mealId}`, { method: "GET" }).then((res) => {
       this.setState({ restaurantList: res.data });
     });
--- React/zomato-app/src/components/details/Details.js
+++ React/zomato-app/src/components/details/Details.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import React, { Component } from "react";
 import "./Details.css";
+import MenuList from "./MenuList";
 const url = "http://localhost:8080";
 
 export default class Details extends Component {
@@ -9,8 +13,14 @@ export default class Details extends Component {
     this.state = {
       details: "",
       menuList: "",
+      userItem: "",
+      mealId: sessionStorage.getItem("mealId"),
     };
   }
+
+  addToCart = (data) => {
+    this.setState({ userItem: data });
+  };
 
   render() {
     // let details = this.state.details
@@ -47,6 +57,37 @@ export default class Details extends Component {
               />
               <figcaption>Fully Sanitized</figcaption>
             </figure>
+          </div>
+          <Tabs>
+            <TabList>
+              <Tab>About Us</Tab>
+              <Tab>Contact Us</Tab>
+            </TabList>
+            <TabPanel>
+              <p>{details.restaurant_name}</p>
+            </TabPanel>
+            <TabPanel>
+              <h3>{details.address}</h3>
+              <p>Contact No: {details.contact_number}</p>
+            </TabPanel>
+          </Tabs>
+          <div>
+            <Link
+              to={`/listing/${this.state.mealId}`}
+              className="btn btn-danger"
+            >
+              BACK
+            </Link>
+            <button className="btn btn-success">Proceed</button>
+          </div>
+
+          <div className="col-md-12">
+            <MenuList
+              menudata={this.state.menuList}
+              finalOrder={(data) => {
+                this.addToCart(data);
+              }}
+            />
           </div>
         </div>
       </div>
